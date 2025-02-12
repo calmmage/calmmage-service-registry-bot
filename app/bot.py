915cdaed77f5bd401f31c22895dfae3b81be84c7
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-02-12 08:34:13 +0300

    Remove broken unused part

--- app/bot.py
+++ app/bot.py
@@ -11,13 +11,11 @@ from botspot.core.bot_manager import BotManager
 # Load environment variables
 load_dotenv(Path(__file__).parent.parent / ".env")
 
-from .routers.settings import router as settings_router
 from .router import app, router as main_router
 
 # Initialize bot and dispatcher
 dp = Dispatcher()
 dp.include_router(main_router)
-dp.include_router(settings_router)
 
 
 def main(debug=False) -> None:
--- dev/todo.md
+++
@@ -1,5 +0,0 @@
-
-Populate the template
-- [ ] add advanced code usage examples - for all features - or just the link to the repo
-  - [ ] add to example.env
-  - [ ] add to README.md - link to github examples dir
